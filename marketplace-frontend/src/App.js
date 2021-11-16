fc4874c0117fa7d30c681388ba4aea643a03fd22
justinrivera47 <justinrivera47@gmail.com>
2021-11-16 16:20:26 -0500

    Making changes to categories

--- marketplace-frontend/src/App.js
+++ marketplace-frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+//Importing my changes from Categories
+import Categories from './components/Categories'
 
 function App() {
   return (
     <div className="App">
       <h1>African Marketplace</h1>
+      <Categories />
     </div>
   );
 }
---
+++ marketplace-frontend/src/components/Categories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <>
+      <div className="header-categories">
+        <header>
+        <i class="fas fa-home"></i>
+        </header>
+      </div>
+      <nav>
+        <h1>Menu</h1>
+        <a href="/">Home</a>
+        <a href="">About</a>
+        <a href="">Products</a>
+        <a href="">Contact</a>
+      </nav>
+    </>
+  )
+}
+
+export default Categories
