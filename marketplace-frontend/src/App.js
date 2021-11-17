416c39486aeb45d6466fee053d609862c95aa2d0
Nithya Narayanan <n.nithya@pm.me>
2021-11-16 19:46:35 -0800

    Completed Homepage routing.

--- marketplace-frontend/src/App.js
+++ marketplace-frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Home from './components/Home'
-
-
+//import About from './components/About'
+import { Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    
-      <Home />
-  );
+    <div>
+      <Route exact path="/">
+          <Home />
+      </Route>
+
+    </div>
+  )
 }
 
 export default App;
