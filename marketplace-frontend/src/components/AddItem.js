b4f601def06a6a049f22ab50147b57d4a47f5824
Bret Banger <Banger484@gmail.com>
2021-11-17 22:11:41 -0600

    worked on add item form

--- marketplace-frontend/src/components/AddItem.js
+++ marketplace-frontend/src/components/AddItem.js
@@ -5,32 +5,44 @@ const AddItem = () => {
     <div className="addItemListing">
       <form className="addItemForm">
         <h2>Add Item Listing</h2>
-        <h3 className="addFormText">Name:</h3>
-        <input type="text" className="addInputs" />
-        <h3 className="addFormText">Description:</h3>
-        <input type="text" className="addInputs" />
-        <h3 className="addFormText">Market Location:</h3>
-        <select className="addInputs">
-          <option value="default">---</option>
-          <option value="California">California</option>
-          <option value="Florida">Florida</option>
-          <option value="New York">New York</option>
-          <option value="Texas">Texas</option>
-          <option value="Utah">Utah</option>
-          <option value="Wisconsin">Wisconsin</option>
-        </select>
-        <h3 className="addFormText">Category:</h3>
-        <select className="addInputs">
-          <option value="default">---</option>
-          <option value="Animal Products">Animal Products</option>
-          <option value="Beans">Beans</option>
-          <option value="Cereals">Cereals</option>
-          <option value="Fruits">Fruits</option>
-          <option value="Other">Other</option>
-          <option value="Roots and Tubers">Roots and Tubers</option>
-        </select>
-        <h3 className="addFormText">Price: $</h3>
-        <input type="integer" className="addInputs" />
+        <section className="addItemSection">
+          <div className="inputContainers">
+            <p className="addFormText">Name:</p>
+            <input type="text" className="addInputs" />
+          </div>
+          <div className="inputContainers">
+            <p className="addFormText">Description:</p>
+            <input type="text" className="addInputs" />
+          </div>
+          <div className="inputContainers">
+            <p className="addFormText">Market Location:</p>
+            <select className="addInputs">
+              <option value="default">---</option>
+              <option value="California">California</option>
+              <option value="Florida">Florida</option>
+              <option value="New York">New York</option>
+              <option value="Texas">Texas</option>
+              <option value="Utah">Utah</option>
+              <option value="Wisconsin">Wisconsin</option>
+            </select>
+          </div>
+          <div className="inputContainers">
+            <p className="addFormText">Category:</p>
+            <select className="addInputs">
+              <option value="default">---</option>
+              <option value="Animal Products">Animal Products</option>
+              <option value="Beans">Beans</option>
+              <option value="Cereals">Cereals</option>
+              <option value="Fruits">Fruits</option>
+              <option value="Other">Other</option>
+              <option value="Roots and Tubers">Roots and Tubers</option>
+            </select>
+          </div>
+          <div className="inputContainers">
+            <p className="addFormText">Price: $</p>
+            <input type="integer" className="addInputs" />
+          </div>
+        </section>
       </form>
     </div>
   );
