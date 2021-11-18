6ba8679d1082f4d5d33a3f3043be564d0c5e18ba
cc <votaryofturms@icloud.com>
2021-11-18 15:07:24 -0500

    changed Category to individual catergory components

---
+++ marketplace-frontend/src/components/CatergoryComponents/Cereals.js
@@ -0,0 +1,31 @@
+
+
+
+
+const Cereals = () => {
+
+    return (
+        <div>
+            <div>
+                <h3>Product Name: Imported Rice</h3>
+                <p>Location: Democratic Republic of Congo</p>
+                <p>Price: 2 CDF</p>
+                <p>Description: Asian imported japonica rice.</p>
+            </div>
+            <div>
+                <h3>Product Name: Maize</h3>
+                <p>Location: Rwanda</p>
+                <p>Price: 2 RWF</p>
+                <p>Description: Organic maize, perfect for a variety of dishes.</p>
+            </div>
+            <div>
+                <h3>Product Name: Barley</h3>
+                <p>Location: Burundi</p>
+                <p>Price: 2 BIF</p>
+                <p>Description: Organic barley for culinary use or disitilling spirits.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Cereals
